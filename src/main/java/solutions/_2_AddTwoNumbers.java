c323abd4902a0018d4e9065a1ad4d3baaa4e2e6f
Kranthi Kiran Kalletla <kranthikiran.kalletla@gmail.com>
2019-06-29 15:26:28 -0700

    Updated solution for Problem 2

--- src/main/java/solutions/_2_AddTwoNumbers.java
+++ src/main/java/solutions/_2_AddTwoNumbers.java
@@ -18,61 +18,24 @@ public class _2_AddTwoNumbers {
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 
-        ListNode head = null;
+        ListNode head = new ListNode(Integer.MIN_VALUE);
 
         int carry = 0;
-        int sum = (l1 != null? l1.val : 0)  + (l2 != null? l2.val : 0)  + carry;
-        carry = sum / 10;
-        sum %= 10;
-        head = new ListNode(sum);
+
         ListNode currentNode = head;
-        l1 = l1.next; l2 = l2.next;
+        while(l1 != null  || l2 != null) {
 
-
-        while(l1 != null  && l2 != null) {
-
-            sum = l1.val + l2.val + carry;
+            int sum = (l1 != null? l1.val : 0) + (l2 != null? l2.val : 0) + carry;
             carry = sum / 10;
             sum %= 10;
             currentNode.next = new ListNode(sum);
-            l1 = l1.next; l2 = l2.next; currentNode = currentNode.next;
+            l1 = l1 != null ? l1.next : null;
+            l2 = l2 != null ? l2.next : null;
+            currentNode = currentNode.next;
         }
-
-        while(l1 != null) {
-            sum = l1.val + carry;
-            carry = sum / 10;
-            sum %= 10;
-            currentNode.next = new ListNode(sum);
-            l1 = l1.next; currentNode = currentNode.next;
-        }
-
-        while(l2 != null) {
-            sum = l2.val + carry;
-            carry = sum / 10;
-            sum %= 10;
-            currentNode.next = new ListNode(sum);
-            l2 = l2.next; currentNode = currentNode.next;
-        }
-
-        /*carry  = this.addRemainingElements(currentNode, l1, carry);
-
-        carry = this.addRemainingElements(currentNode, l2, carry);*/
 
         if (carry != 0)
             currentNode.next = new ListNode(carry);
-        return head;
-    }
-
-    private int addRemainingElements(ListNode currentNode, ListNode element, int carry) {
-
-        while(element != null) {
-            int sum = element.val + carry;
-            carry = sum / 10;
-            sum %= 10;
-            currentNode.next = new ListNode(sum);
-            element = element.next; currentNode = currentNode.next;
-        }
-
-        return carry;
+        return head.next;
     }
 }
