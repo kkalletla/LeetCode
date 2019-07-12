57676f0c1c370028339d0b948fe222f5a22a7a16
Kranthi Kiran Kalletla <kranthikiran.kalletla@gmail.com>
2019-07-12 18:52:47 -0400

    Added solution for PlusOne

---
+++ src/main/java/solutions/topInterviewQuestions/arrays/PlusOne.java
@@ -0,0 +1,40 @@
+package solutions.topInterviewQuestions.arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlusOne {
+
+    public static void main(String[] args) {
+
+        for (int i: new PlusOne().plusOne(new int[] {9})) {
+            System.out.println(i);
+        }
+    }
+
+    public int[] plusOne(int[] digits) {
+
+        if (digits == null || digits.length == 0)
+            return digits;
+
+        List<Integer> number = new ArrayList<Integer>();
+        int carry = 1;
+        int sum;
+        for (int i = digits.length -1; i > -1; i--) {
+            sum = digits[i] + carry;
+            carry = sum / 10;
+            number.add(sum%10);
+        }
+
+        if (carry > 0) {
+            number.add(carry);
+        }
+
+        int[] plusOneNumber = new int[number.size()];
+        for (int i = 0; i < number.size(); i++) {
+            plusOneNumber[i] = number.get(number.size() - i - 1);
+        }
+
+        return plusOneNumber;
+    }
+}
