6d2b1c3702a719e491d4f7f1daa403c60b99b2da
Kranthi Kiran Kalletla <kranthikiran.kalletla@gmail.com>
2019-07-21 16:53:41 -0400

    Added code for ValidSudoku.java

---
+++ src/main/java/solutions/topInterviewQuestions/arrays/ValidSudoku.java
@@ -0,0 +1,88 @@
+package solutions.topInterviewQuestions.arrays;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ValidSudoku {
+
+    Map<Character, Integer> characterMap = new HashMap<Character, Integer>();
+
+
+    public static void main(String[] args) {
+
+        char[][] board = new char[][] {
+                {'8','3','.','.','7','.','.','.','.'},
+                {'6','.','.','1','9','5','.','.','.'},
+                {'.','9','8','.','.','.','.','6','.'},
+                {'8','.','.','.','6','.','.','.','3'},
+                {'4','.','.','8','.','3','.','.','1'},
+                {'7','.','.','.','2','.','.','.','6'},
+                {'.','6','.','.','.','.','2','8','.'},
+                {'.','.','.','4','1','9','.','.','5'},
+                {'.','.','.','.','8','.','.','7','9'}
+        };
+
+        System.out.println(new ValidSudoku().isValidSudoku(board));
+
+    }
+
+    public boolean isValidSudoku(char[][] board) {
+
+        characterMap.put('.', 0);
+        characterMap.put('1', 1);
+        characterMap.put('2', 2);
+        characterMap.put('3', 3);
+        characterMap.put('4', 4);
+        characterMap.put('5', 5);
+        characterMap.put('6', 6);
+        characterMap.put('7', 7);
+        characterMap.put('8', 8);
+        characterMap.put('9', 9);
+
+        return validateRowsAndColumns(board) && validate3X3Grid(board);
+    }
+
+    private boolean validateRowsAndColumns(char[][] board) {
+
+        for (int i = 0; i < 9; i++) {
+            int[] numbersCounterForRows = new int[10];
+            int[] numbersCounterForColumns = new int[10];
+            for (int j = 0; j < 9; j++) {
+                numbersCounterForRows[characterMap.get(board[i][j])]++;
+                numbersCounterForColumns[characterMap.get(board[j][i])]++;
+            }
+
+            for (int k = 1; k < 10; k++)
+                if (numbersCounterForRows[k] > 1 || numbersCounterForColumns[k] > 1)
+                    return false;
+        }
+        return true;
+    }
+
+    private boolean validate3X3Grid(char[][] board) {
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                int[] numbersCounterForGrid = new int[10];
+
+                for (int l = 0; l < 3; l++) {
+                    for (int m = 0; m < 3; m++) {
+                        numbersCounterForGrid[characterMap.get(board[i*3+l][j*3+m])]++;
+                    }
+                }
+
+                for (int k = 1; k < 10; k++)
+                    if (numbersCounterForGrid[k] > 1)
+                        return false;
+            }
+        }
+        return true;
+    }
+
+    private void printArray(int[] counters) {
+        for (int i: counters)
+            System.out.print(i);
+        System.out.println();
+    }
+
+}
