e1dbe694c60b487c5588563bc8e4d075add37c1d
Kranthi Kiran Kalletla <kranthikiran.kalletla@gmail.com>
2019-09-08 10:31:13 -0400

    Added solution for 257

--- src/main/java/solutions/_257_BinaryTreePaths.java
+++ src/main/java/solutions/_257_BinaryTreePaths.java
@@ -1,6 +1,28 @@
 package solutions;
 
-public class _257_BinaryTreePaths {
+import Utilities.TreeNode;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class _257_BinaryTreePaths {
     
+    List<String> paths = new ArrayList<>();
+    public List<String> binaryTreePaths(TreeNode root) {
+        binaryTreePaths(new String(), root);
+
+        return paths;
+    }
+
+    public void binaryTreePaths(String path, TreeNode root) {
+
+        if (root == null)
+            return;
+
+        if (root != null && root.left == null && root.right == null)
+            paths.add(path + (Integer.toString(root.val)));
+
+        binaryTreePaths(path + Integer.toString(root.val) + "->", root.left);
+        binaryTreePaths(path + Integer.toString(root.val) + "->", root.right);
+    }
 }
