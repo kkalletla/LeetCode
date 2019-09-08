0d08371e946af8327b8a3c61ad39eb60375da9bc
Kranthi Kiran Kalletla <kranthikiran.kalletla@gmail.com>
2019-09-08 16:14:42 -0400

    Updated solution for 113

--- src/main/java/solutions/_113_PathSumII.java
+++ src/main/java/solutions/_113_PathSumII.java
@@ -9,25 +9,6 @@ import java.util.List;
 
 public class _113_PathSumII {
 
-    public static void main(String[] args) {
-
-        TreeNode root = new TreeNode(5);
-        root.left = new TreeNode(4);
-        root.right = new TreeNode(8);
-
-        root.left.left = new TreeNode(11);
-        root.left.left.left = new TreeNode(7);
-        root.left.left.right = new TreeNode(2);
-
-        root.right.left = new TreeNode(13);
-        root.right.right = new TreeNode(4);
-        root.right.right.left = new TreeNode(5);
-        root.right.right.right = new TreeNode(1);
-
-        System.out.println(new _113_PathSumII().pathSum(root, 22));
-
-    }
-
     public List<List<Integer>> pathSum(TreeNode root, int sum) {
 
         List<List<Integer>> sums = new LinkedList<>();
